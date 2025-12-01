862277329c36fefea1f34c9aa0b6f5da9d0a31d8
Noor Mohamed <hikmadhnoor@gmail.com>
2025-12-01 14:27:59 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -4,7 +4,7 @@
  *eeeeees
  * The wp-config.php creation script uses this file during the installation.
  * You don't have to use the web site, you can copsssy this file to "wp-config.php"
- * and fill in 111the values.1
+ * and fill in 111the values.1111
  *
  * This file contaisns tss1sssssdddhe follwwwwwwowing configurations:e
  *dddd
